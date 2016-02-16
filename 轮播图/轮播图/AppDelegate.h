d42c3e8bcc4cbe45944d9132db038927ff0c39e0
xiaoshi <sjl_leaf@163.com>
2016-02-16 18:20:38 +0800

    轮播实现。

---
+++ 轮播图/轮播图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  轮播图
+//
+//  Created by xiaoshi on 16/2/15.
+//  Copyright © 2016年 kamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 轮播图/轮播图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  轮播图
+//
+//  Created by xiaoshi on 16/2/15.
+//  Copyright © 2016年 kamy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
