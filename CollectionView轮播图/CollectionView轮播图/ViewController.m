71b46fa9980d7969d156a4a49508ea8ed3ee048c
xiaoshi <sjl_leaf@163.com>
2016-02-17 14:59:47 +0800

    init CollectionView轮播图

---
+++ CollectionView轮播图/CollectionView轮播图/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CollectionView轮播图
+//
+//  Created by xiaoshi on 16/2/17.
+//  Copyright © 2016年 kamy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
